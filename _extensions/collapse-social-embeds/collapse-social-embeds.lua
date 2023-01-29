7f65865253d7a0fa97b4fd0a55b11ebd837ad7c8
shafayetShafee <seaborn.sicario@gmail.com>
2023-01-29 19:12:16 +0600

    added support for rest of the embeds

--- _extensions/collapse-social-embeds/collapse-social-embeds.lua
+++ _extensions/collapse-social-embeds/collapse-social-embeds.lua
@@ -1,15 +1,30 @@
--- based on the source code from https://github.com/sellorm/quarto-social-embeds
-
-local function ensureHtmlDeps()
-  quarto.doc.add_html_dependency({
-  name = "collapse-social-embeds",
-  version = "1.0.0",
-  stylesheets = {"resources/css/collapse.css"}
-})
-end
+-- the code for embedding social contents were taken 
+-- from https://github.com/sellorm/quarto-social-embeds 
 
 local str = pandoc.utils.stringify
 local p = quarto.log.output
+
+
+local function ensureHtmlDeps()
+  quarto.doc.add_html_dependency({
+      name = "collapse-social-embeds",
+      version = "1.0.0",
+      stylesheets = {"resources/css/collapse.css"}
+    },
+    {
+      name = 'twitter',
+      version = '0.0.1',
+      scripts = {
+          { 
+            path = "",
+            attribs = {src="https://platform.twitter.com/widgets.js"},
+            afterBody = true
+          }
+      }
+    }
+  )
+end
+
 
 function embed_gist()
   return {
@@ -85,13 +100,14 @@ function embed_loom()
   }
 end
 
+
 function embed_mastodon()
   return {
     Div = function(el)
       if el.classes:includes('mastodon') 
       then
         local attr = el.attributes
-        local mstd_url = attr['url']
+        local mstd_url = str(attr['url'])
         -- Assemble HTML to be returned
         local html = '<div style="position: relative; padding-bottom: 56.25%; height: 0;"> <iframe src="'.. mstd_url ..'/embed" class="mastodon-embed" style="max-width: 100%; border: 0;" width="100%" height="400" allowfullscreen="allowfullscreen"></iframe><script src="https://mastodon.social/embed.js" async="async"></script></div>'
 	      local mstd = pandoc.RawInline('html', html)
@@ -114,12 +130,125 @@ function embed_mastodon()
 end
 
 
+function embed_tweet()
+  return {
+     Div = function(el)
+       if el.classes:includes('twitter')
+       then
+         local attr = el.attributes
+         local user = attr.user
+         local status_id = attr.status_id
+         -- Assemble the twitter oembed API URL from the user inputs
+         local tweet_embed = 'https://publish.twitter.com/oembed?url=https://twitter.com/' 
+                .. user
+                .. '/status/'
+                .. status_id
+                .. '&align=center'
+         local mt, api_resp = pandoc.mediabag.fetch(tweet_embed)
+            
+         -- generate a random number to append to the html div ID to avoid re-use
+         local id = math.random(10000, 99999)
+      
+         local tweet_data = '<div id="tweet-'
+            .. id 
+            .. '"></div><script>tweet=' 
+            .. api_resp 
+            .. ';document.getElementById("tweet-' 
+            .. id 
+            .. '").innerHTML = tweet["html"];</script>'
+      
+         local tweet = pandoc.RawInline('html', tweet_data)
+         local icon = attr['icon'] or true
+         local collapse_appearance = "default"
+         if attr['appearance'] then
+           collapse_appearance = attr['appearance']
+         end
+         local caption = "Tweet from " .. user
+         return pandoc.Div({quarto.Callout({
+          type = "note",
+          icon = icon,
+          collapse = true,
+          caption = caption,
+          content =  { tweet },
+          appearance = collapse_appearance
+          })}, {class = 'tweet'})
+      end       
+    end
+  }
+end
+
+
+function embed_vimeo()
+  return {
+    Div = function(el)
+      if el.classes:includes('vimeo')
+      then
+        local attr = el.attributes
+        local video_id = attr.video_id
+        -- Assemble HTML to be returned
+        local html = '<div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden;"> <iframe src="https://player.vimeo.com/video/'
+          .. video_id
+          .. '" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; border:0;" title="vimeo video" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> </div> '
+
+	      local vimeo_video = pandoc.RawInline('html', html)
+        local icon = attr['icon'] or true
+        local collapse_appearance = "default"
+        if attr['appearance'] then
+          collapse_appearance = attr['appearance']
+        end
+        return pandoc.Div({quarto.Callout({
+          type = "note",
+          icon = icon,
+          collapse = true,
+          caption = "Vimeo Video",
+          content =  { vimeo_video },
+          appearance = collapse_appearance
+        })}, {class = 'vimeo'})
+      end    
+    end
+  }
+end
+
+
+function embed_yt()
+  return {
+    Div = function(el)
+      if el.classes:includes('youtube')
+      then
+        local attr = el.attributes
+        local video_id = attr.video_id
+        -- Assemble HTML to be returned
+        local html = '<div id="youtube-frame" style="position: relative; padding-bottom: 56.25%; /* 16:9 */ height: 0;"><iframe width="100%" height="" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="https://www.youtube.com/embed/'
+          .. video_id
+          .. '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>'
+	      local yt_video = pandoc.RawInline('html', html)
+	      local icon = attr['icon'] or true
+	      local collapse_appearance = "default"
+        if attr['appearance'] then
+          collapse_appearance = attr['appearance']
+        end
+        return pandoc.Div({quarto.Callout({
+          type = "note",
+          icon = icon,
+          collapse = true,
+          caption = "Youtube Video",
+          content =  { yt_video },
+          appearance = collapse_appearance
+        })}, {class = 'youtube'})
+      end
+    end
+  }
+end
+
+
 function Pandoc(doc)
   if quarto.doc.isFormat('html') then
     ensureHtmlDeps()
     local doc = doc:walk(embed_gist())
     doc = doc:walk(embed_loom())
     doc = doc:walk(embed_mastodon())
-    return doc
+    doc = doc:walk(embed_tweet())
+    doc = doc:walk(embed_vimeo())
+    return doc:walk(embed_yt())
   end
 end
